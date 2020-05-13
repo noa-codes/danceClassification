236194cf3aa97f7a3bb0d028cf60561ac58f188e
Noa Bendit-Shtull <noa.f.bs@gmail.com>
2020-05-13 17:58:36 +0000

    [data] Debug RGB dataset loading
    * Reset index on each train / test/ val index file
    * Convert tensor X to numpy array prior to saving
    * Make corrections to paths

--- dataset.py
+++ dataset.py
@@ -32,7 +32,7 @@ def get_splits(vids):
   return (train_vids, val_vids, test_vids)
 
 
-def preprocessRGB(raw_dataset_path, resize_dims):
+def preprocessRGB(raw_dataset_path, resize_dims=[720,1280]):
   """ 
   Preprocess RGB files in the raw dataset path and 
   save an index of the contents to CSV
@@ -41,13 +41,11 @@ def preprocessRGB(raw_dataset_path, resize_dims):
   @param resize_dims Dimensions to resize raw images [height, width]
   """ 
   processed_dataset_path = get_processed_dataset_path(raw_dataset_path)
-  if resize_dims is None:
-    resize_dims = [720, 1280]
-
+    
   # list files in folder
-  files = list()
+  rgb_files = list()
   for (dirpath, dirnames, filenames) in os.walk(raw_dataset_path):
-    files += [os.path.join(dirpath, file) for file in filenames]
+    rgb_files += [os.path.join(dirpath, file) for file in filenames]
 
   # create pandas data frame with RGB data
   rgb = pd.DataFrame(rgb_files, columns=["filename"])
@@ -67,19 +65,18 @@ def preprocessRGB(raw_dataset_path, resize_dims):
   rgb.reset_index(drop=True, inplace=True)
 
   # add filepath for processed data
-  rgb['processed_path'] = os.path.join(
-    processed_dataset_path, 
-    os.path.basename(rgb['filename'])[:-4], ".npy")
-
+  rgb['processed_path'] = rgb['filename'].apply(lambda x: os.path.join(
+    processed_dataset_path, f"{os.path.basename(x)[:-4]}.npy"))
+  
   # split to train, val, test, and save file indexes
   train_vids, val_vids, test_vids = get_splits(rgb['vid'].drop_duplicates())
-  train = rgb[rgb['vid'].isin(train_vids)]
-  val = rgb[rgb['vid'].isin(val_vids)]
-  test = rgb[rgb['vid'].isin(test_vids)]
-  train.to_csv("data/rgb_train_index.csv")
-  val.to_csv("data/rgb_val_index.csv")
-  test.to_csv("data/rgb_test_index.csv")
-
+  train = rgb[rgb['vid'].isin(train_vids)].reset_index(drop=True)
+  val = rgb[rgb['vid'].isin(val_vids)].reset_index(drop=True)
+  test = rgb[rgb['vid'].isin(test_vids)].reset_index(drop=True)
+  train.to_csv(os.path.join(os.path.dirname(processed_dataset_path),"rgb_train_index.csv"))
+  val.to_csv(os.path.join(os.path.dirname(processed_dataset_path),"rgb_val_index.csv"))
+  test.to_csv(os.path.join(os.path.dirname(processed_dataset_path),"rgb_test_index.csv"))
+    
   # create a transform 
   transform = transforms.Compose([
       transforms.Resize(resize_dims),
@@ -89,14 +86,14 @@ def preprocessRGB(raw_dataset_path, resize_dims):
         std=[0.229, 0.224, 0.225])])
 
   # transform raw data and save processed files
-  for i, fpath in enumerate(files):
+  for i, fpath in enumerate(rgb_files):
     # load and transform image
     X = Image.open(fpath)
     X = transform(X)
     # get class
     y = rgb["dance_id"][i]
     # save to file
-    obs = np.asarray([X, y])
+    obs = np.asarray([X.numpy(), y])
     np.save(rgb['processed_path'][i], obs, allow_pickle=True)
 
 
