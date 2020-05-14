7848bfd9a0aabbc85529d301a1d4ac49f8a0d3f9
Noa Bendit-Shtull <noa.f.bs@gmail.com>
2020-05-14 05:24:48 +0000

    [infra] Add command-line arguments and initialize datasets/dataloaders

--- run.py
+++ run.py
@@ -24,12 +24,21 @@ def argParser():
         > python run.py --gpu 0
         args.gpu <-- 0
     """
-    pass
     parser = argparse.ArgumentParser()
 
-    ### ADD ARGUMENTS, for example:
-    ### parser.add_argument("--mode", dest="mode", default='train', help="Mode is one of 'train', 'test', 'generate'")
-
+    # model specifications
+    parser.add_argument("--mode", dest="mode", default='train', help="Mode is one of 'train', 'test'")
+    parser.add_argument("--model", dest="model", default="baseline_lstm", help="Name of model to use")
+    parser.add_argument("--preprocess", dest="preprocess", default=0, help="Preprocess is 0 or 1, default 0")
+    parser.add_argument("--gpu", dest="gpu", type=str, default='0', help="The gpu number if there's more than one gpu")
+    parser.add_argument("--batch-size", dest="batch_size", type=int, default=100, help="Size of the minibatch")
+    
+    # dataset and logger paths
+    parser.add_argument("--train-path", dest="train_path", help="Training data file")
+    parser.add_argument("--val-path", dest="val_path", help="Validation data file")
+    parser.add_argument("--log", dest="log", default='', help="Unique log directory name under log/. If the name is empty, do not store logs")
+    
+    # create argparser
     args = parser.parse_args()
     return args
 
@@ -47,20 +56,33 @@ def main():
     # setup
     print("Setting up...")
     args = argParser()
-    args.is_stream = True if args.is_stream == 1 else False
     device = torch.device('cuda:' + args.gpu if torch.cuda.is_available() else "cpu")
-    unique_logdir = create_unique_logdir(args.log, args.lr)
-    logger = Logger(unique_logdir) if args.log != '' else None
     print("Using device: ", device)
-    print("All training logs will be saved to: ", unique_logdir)
-    print("Will log to tensorboard: ", logger is not None)
+    
+#  Set up logging ----------------------------------------------
+#     unique_logdir = create_unique_logdir(args.log, args.lr)
+#     logger = Logger(unique_logdir) if args.log != '' else None
+#     print("All training logs will be saved to: ", unique_logdir)
+#     print("Will log to tensorboard: ", logger is not None)
 
-    # build dataset object
-    print("Creating Dataset...")
+    # Prepare for forward pass on CNN
+    if args.preprocess:
+ 
+        print("Indexing JPG files...")
+        make_jpg_index("/mnt/disks/disk1/raw/rgb")
+        
+        # build Dataset and DataLoader objects
+        print("Creating Datasets...")
+        train_dataset = cnnDataset(args.train_path)
+        val_dataset = cnnDataset(args.val_path)
 
-    ###### TO DO #######
-    # BUILD DATASETS  ##
-    ####################
+        train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+        val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+
+    # Many-to-one or RNN model
+    else: 
+        pass
+    
 
     # Turns args into a dictionary to pass to models
     kwargs = vars(args) 
