d253c7a91e040fec008f6dd5aa20e79799b90d91
julie <juliewang.jsw@gmail.com>
2020-05-13 21:18:57 -0700

    make model chooser with resnet

--- model.py
+++ model.py
@@ -1,14 +1,16 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torchvision.models as models
 
 def ModelChooser(model_name, **kwargs):
     """
     This function takes in a model name and returns its corresponding model
     Example usage:
-    
+
         if model_name == "baseline_lstm":
             return LSTMLanguageModel(**kwargs)
     """
-    pass 
+    if model_name == "resnet18":
+        resnet18 = models.resnet18(pretrained=True, progress=True, **kwargs)
+        return resnet18
