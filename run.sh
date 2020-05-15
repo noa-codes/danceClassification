e882910b99b7d6d972cfd3ad393192a64e7f45d4
Noa Bendit-Shtull <noa.f.bs@gmail.com>
2020-05-15 04:21:37 +0000

    [infra] Update command to skip encoding stage

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [ "$1" = "test_encoding" ]; then
-    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode 1 --model baseline_lstm --train-path=/mnt/disks/disk1/processed/rgb_train_index.csv --val-path=/mnt/disks/disk1/processed/rgb_val_index.csv --batch-size 100
+if [ "$1" = "debug" ]; then
+    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --model baseline_lstm --train-path=/mnt/disks/disk1/processed/rgb/rgb_train_index.csv --val-path=/mnt/disks/disk1/processed/rgb/rgb_val_index.csv --encode-path=/mnt/disks/disk1/processed/rgb --batch-size 100
 fi
