31b0dc5f559759bca22d3001eb8c1b7340ea8a90
Julie Wang <julie.wang@stanford.edu>
2020-05-16 01:09:34 +0000

    [model]     * Added Pose extraction model     * Started filling in test function, not tested

--- model.py
+++ model.py
@@ -3,6 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 
+# Flatten helper layer
+class Flatten(nn.Module):
+        def forward(self, x):
+                    return flatten(x)
+
 def ModelChooser(model_name, **kwargs):
     """
     This function takes in a model name and returns its corresponding model
@@ -17,4 +22,14 @@ def ModelChooser(model_name, **kwargs):
         model = models.resnet18(pretrained=True)
         #Replace that final fc with identity layer
         model.fc = nn.Identity()
+        return model
+
+    #Mini ConvNet to train on densepose features
+    if model_name == "pose_features":
+        model = nn.Sequential(
+            nn.Conv2d(20, 64, 3, padding=1), #input: 20x2x17, output: 64x2x17
+            nn.Conv2d(64, 32, 3, padding=1), #input: 64x2x17, output: 32x2x17
+            Flatten(),
+            nn.Linear(32*2*17, 512),
+        )
         return model
--- run.py
+++ run.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import torchvision.models as models
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
+import torch.nn.functional as F
 
 from utils import *
 from model import *
@@ -46,9 +47,6 @@ def argParser():
     args = parser.parse_args()
     return args
 
-def train():
-    pass
-
 
 def main():
     """
@@ -72,12 +70,11 @@ def main():
     params = kwargs.copy()
 
     # Encode your data before using it
-    encode_path_train = os.path.join(args.encode_path, "encoded_features_train.pt")
-    encode_path_val = os.path.join(args.encode_path, "encoded_features_val.pt")
-    
     if args.encode == 1:
         print("Starting encoding...")
-        
+        encode_path_train = os.path.join(args.encode_path, "encoded_features_train.pt")
+        encode_path_val = os.path.join(args.encode_path, "encoded_features_val.pt")
+
         # indexes don't exist, create them
         if not os.path.exists(args.train_path):
             make_jpg_index("/mnt/disks/disk1/raw/rgb")
@@ -88,43 +85,72 @@ def main():
         val_dataset = cnnDataset(args.val_path)
         val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
         
-        # initialize encoder model
-        encoding_model = ModelChooser("resnet18_features")
-        encoding_model = encoding_model.to(device)
-        
+        # Forward pass through the RGB CNN encoding model
+        rgb_encoder = ModelChooser("resnet18_features")
+        rgb_encoder = rgb_encrgb_encoderoding_model.to(device)
         # Run a test forward pass to save all features
-        print("Computing CNN forward pass...")
-        test(encoding_model, dataloader, device, save_filepath=encode_path_train)
-        test(encoding_model, val_dataloader, device, save_filepath=encode_path_val)
+        print("Computing RGB CNN forward pass...")
+        test(rgb_encoder, dataloader, device, save_filepath=encode_path_train)
+        test(rgb_encoder, val_dataloader, device, save_filepath=encode_path_val)
+
+        # Train the Densepose CNN encoding model
+#         pose_encoder = ModelChooser("pose_features")
+#         pose_encoder = pose_encoder.to(device)
+#         print("Computing Pose CNN forward and backward passes...")
+#         # TODO, put the dataloader for the pose data here
+#         pose_dataset =
+#         pose_dataloader =
+#         optimizer = optim.SGD(model.parameters(), lr=.01,
+#                      momentum=0.9, nesterov=True)
+#         train(pose_encoder, optimizer, pose_dataloader, device)
+
 
     # Load the model
     model = ModelChooser(args.model, **kwargs)
     model = model.to(device)
 
     # Load the encoded feature dataset
+    # TODO: concatenate the RGB and pose data
     frame_select = range(0,300,5)
     dataset = rnnDataset(encode_path_train, args.train_path, frame_select)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
     val_dataset = rnnDataset(encode_path_val, args.val_path, frame_select)
     val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
 
+
     if args.mode == 'train':
         print("Starting training...")
         # Save all params used to train
         json.dump(params, open(os.path.join(unique_logdir, "params.json"), 'w'), indent=2)
+
+        # TODO: Better way to pick the optimizer
+        optimizer = optim.SGD(model.parameters(), lr=learning_rate,
+                     momentum=0.9, nesterov=True)
         # train model
-        train(...)
+        train(model, optimizer, dataloader, val_dataloader, device)
 
     elif args.mode == 'test':
         print("Starting testing...")
         test(model, dataloader, device)
 
+def train(model, optimizer, dataloader, val_dataloader, device, epochs = 10, dtype=None, **kwargs):
+    for e in range(epochs):
+        for t, (x,y) in enumerate(dataloader):
+            model.train()
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+            scores = model(x)
+            loss = F.cross_entropy(scores, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        print('Epoch %d, loss = %.4f' % (e, loss.item()))
 
-def test(model, dataloader, device, dtype=None, save_filepath=None, **kwargs):
+def test(model, optimizer, dataloader, device, dtype=None, save_filepath=None, **kwargs):
     """
     Test your model on the dataloaded by dataloader
     """
-    encoding = []
+    all_scores = []
     num_correct = 0
     num_samples = 0
     
@@ -143,10 +169,10 @@ def test(model, dataloader, device, dtype=None, save_filepath=None, **kwargs):
 
             # Record scores to save
             if save_filepath is not None:
-                encoding.append(scores)
+                all_scores.append(scores)
 
     if save_filepath:
-        encoding = torch.cat(encoding)
+        encoding = torch.cat(all_scores)
         torch.save(encoding, save_filepath)
     
     # Report accuracy
