de6cb7f8c3322a1ce3bc3c38be9aa96bbf5e961e
Noa Bendit-Shtull <noa.f.bs@gmail.com>
2020-05-21 01:21:46 +0000

    [infra] Add LSTM training paramters as default to run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 if [ "$1" = "pose_encode" ]; then
     CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=3 --batch-size=100 --log=pose --learning-rate=3e-5
+elif [ "$1" = "train_rnn" ]; then
+    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --batch-size=10 --log=lstm --learning-rate=1e-3 --epochs=100
 fi
