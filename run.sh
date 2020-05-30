089c3c750928cc3ef074527a99424bce66174a5d
Noa Bendit-Shtull <noa.f.bs@gmail.com>
2020-05-30 21:18:39 +0000

    [infra] Add shorthand for training TCN model

--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ elif [ "$1" = "train_rnn" ]; then
     CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --batch-size=20 --log=lstm --learning-rate=1e-3 --epochs=200
 elif [ "$1" = "test_rnn" ]; then
     CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --batch-size=100 --mode=test --checkpoint=/mnt/disks/disk1/log/lstm_2020_5_21_h17_m6_lr0.001/checkpoints/best_val_loss.pth
+elif [ "$1" = "train_tcn" ]; then
+    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --batch-size=20 --log=tcn --learning-rate=1e-3 --epochs=500 --hidden-size=25 --levels 3 --optim=Adam --dropout 0.25
 fi
