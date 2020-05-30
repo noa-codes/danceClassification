db029f8af966e45d632faea74b7004abc1651cba
Noa Bendit-Shtull <noa.f.bs@gmail.com>
2020-05-30 13:13:53 -0700

    [model]
    * Add TCN model from Bai et al (2018)
    * Create a module for PoseCNN, including its weight initialization function
    * Create a constant for the input size (open to changing this)

--- model.py
+++ model.py
@@ -1,17 +1,13 @@
+import collections
+from tcn import TemporalConvNet
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
-import collections
 
 # Constants
 C_NUM_CLASSES = 10
-
-# Helper Functions
-def init_weights(m):
-    if type(m) == nn.Linear or type(m) == nn.Conv2d:
-        torch.nn.init.xavier_uniform_(m.weight)
-        m.bias.data.fill_(0.01)
+C_INPUT_SIZE = 102 + 512
 
 # Helper Classes
 # Flatten helper layer
@@ -38,7 +34,54 @@ class DefaultLSTM(nn.Module):
         scores = self.fc1(x)
         return scores
 
-def ModelChooser(model_name, **kwargs):
+class PoseCNN(nn.Module):
+    """  
+    Taken from https://arxiv.org/pdf/1909.03466.pdf,
+    Multi-Modal Three-Stream Network for Action Recognition
+    Structure: conv-conv-relu-maxpool-fc-softmax
+    """
+    def __init__(self)
+        super(PoseCNN, self).__init__()
+        self.network = nn.Sequential(collections.OrderedDict([
+            ('conv1', nn.Conv2d(20, 3, 3, padding=1)), #input: 20x2x17, output: 3x2x17
+            ('conv2', nn.Conv2d(3, 3, 3, padding=1)), #input: 3x2x17, output: 3x2x17
+            ('flatten', Flatten()),
+            ('relu', nn.ReLU()),
+            ('fcfinal', nn.Linear(102, C_NUM_CLASSES))])
+        )
+        model.apply(init_weights)
+
+    def init_weights(self, m):
+        if type(m) == nn.Linear or type(m) == nn.Conv2d:
+            torch.nn.init.xavier_uniform_(m.weight)
+            m.bias.data.fill_(0.01)
+
+    def forward(self, x):
+        return self.network(x)
+
+
+class TCN(nn.Module):
+    """
+    Temporal Convolutional Network, adapted from Bai et al (2018)
+    https://github.com/locuslab/TCN/blob/master/TCN/mnist_pixel/model.py
+    """
+    def __init__(self, input_size, num_classes, num_channels, kernel_size, dropout):
+        super(TCN, self).__init__()
+        self.tcn = TemporalConvNet(input_size, num_channels, 
+            kernel_size=kernel_size, dropout=dropout)
+        self.linear = nn.Linear(num_channels[-1], num_classes)
+
+    def forward(self, x):
+        # x has dimension (batch_size, seq_length, input_dim)
+        ## TCN requires input of dimenson (batch_size, input_dim, seq_length)
+        x = torch.transpose(x, 1, 2)
+        x = self.tcn(x) 
+        # only keep final TCN output for linear layer
+        scores = self.linear(x[:, :, -1])
+        return scores
+
+
+def ModelChooser(model_name, args):
     """
     This function takes in a model name and returns its corresponding model
     Example usage:
@@ -47,32 +90,29 @@ def ModelChooser(model_name, **kwargs):
             return LSTMLanguageModel(**kwargs)
     """
 
-    #Resnet18 model with identity final layer
+    # Resnet18 model with identity final layer
     if model_name == "resnet18_features":
         model = models.resnet18(pretrained=True)
-        #Replace that final fc with identity layer
+        # Replace that final fc with identity layer
         model.fc = nn.Identity()
         return model
 
-    #Mini ConvNet to train on densepose features
-    # Taken from https://arxiv.org/pdf/1909.03466.pdf
-    # Multi-Modal Three-Stream Network for Action Recognition
-    # conv-conv-relu-maxpool-fc-softmax
+    # Mini ConvNet to train on densepose features
     if model_name == "pose_features":
-        model = nn.Sequential(collections.OrderedDict([
-            ('conv1', nn.Conv2d(20, 3, 3, padding=1)), #input: 20x2x17, output: 3x2x17
-            ('conv2', nn.Conv2d(3, 3, 3, padding=1)), #input: 3x2x17, output: 3x2x17
-            ('flatten', Flatten()),
-            ('relu', nn.ReLU()),
-#             ('pool', nn.MaxPool2d(2, 2)), #input: 3x2x17, 
-            ('fcfinal', nn.Linear(102, C_NUM_CLASSES))])
-        )
-        model.apply(init_weights)
-        return model
+        return PoseCNN()
     
+    # Simple LSTM model
     if model_name == "baseline_lstm":
-        # number of features in input
-        input_size = 102 + 512
-        hidden_size = 100 # TO DO: we should make this a tunable paramter
-        model = DefaultLSTM(input_size, hidden_size, C_NUM_CLASSES)
+        model = DefaultLSTM(C_INPUT_SIZE, args.hidden_size, C_NUM_CLASSES)
+        return model
+
+    # Temporal Convolutional Network
+    if model_name == 'tcn':
+        channel_sizes = [args.hidden_size] * args.levels
+        model = TCN(
+            input_size=C_INPUT_SIZE, 
+            num_classes=C_NUM_CLASSES, 
+            num_channels=channel_sizes, 
+            kernel_size=3, 
+            dropout=args.dropout)
         return model
