9b0ff3f48b9368e5d1fba75e94e27859dbf31fb2
Tiffany Cheng <tiffany.ucb@gmail.com>
2020-06-01 21:57:00 -0700

    [model] Add line to run.sh for the lstm with attention

--- run.sh
+++ run.sh
@@ -8,4 +8,6 @@ elif [ "$1" = "test_rnn" ]; then
     CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --batch-size=100 --mode=test --checkpoint=/mnt/disks/disk1/log/lstm_2020_5_21_h17_m6_lr0.001/checkpoints/best_val_loss.pth
 elif [ "$1" = "train_tcn" ]; then
     CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --batch-size=20 --log=tcn --learning-rate=1e-3 --epochs=500 --hidden-size=25 --levels 3 --optim=Adam --dropout 0.25
+elif [ "$1" = "train_rnn_attention" ]; then
+    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --batch-size=20 --log=lstm_att --learning-rate=1e-3 --epochs=200 --model=attention_lstm
 fi
