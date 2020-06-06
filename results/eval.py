22b18aea0ebce2fe2d970972ea1edc939dbcf26d
Tiffany Cheng <tiffany.ucb@gmail.com>
2020-06-06 10:44:48 +0000

    [infra] Add confusion matrix code + apply it to existing test runs

---
+++ results/eval.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def evaluate_performance(true_y, pred_y, print_vals=True):
+    """
+    Helper function that evaluates a model's performance by outputting accuracy rate and a confusion matrix.
+    """
+    cnf_matrix = metrics.confusion_matrix(true_y, pred_y)
+    
+    class_names=['ballet', 'break', 'flamenco', 'foxtrot', 'latin', 'quickstep', 'square', 'swing', 'tango', 'waltz']
+    cnf_matrix = pd.DataFrame(cnf_matrix, index = class_names,
+                  columns = class_names)
+    
+    # plot confusion matrix with heatmap
+    sns.heatmap(cnf_matrix, annot=True, cmap="YlGnBu" ,fmt='g')
+    plt.tight_layout()
+    plt.title('Confusion matrix', y=1.1)
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
+    
+    if print_vals :
+        n_accurate = (true_y == pred_y).sum()
+        total = len(true_y)
+        acc_rate = n_accurate / total
+        print('Accuracy: {}/{} = {:.4f}'.format(n_accurate, total, acc_rate))
