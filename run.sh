62836983c7d95a8a3329b5a78c961a4b7fab8630
Tiffany Cheng <tiffany.ucb@gmail.com>
2020-06-07 07:34:40 +0000

    [infra] Update run.sh with parameters from hypertuning for lstm with attention

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ if [ "$1" = "pose_encode" ]; then
 elif [ "$1" = "tune_lstm" ]; then
     CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --mode=tune --epochs=500 --model=baseline_lstm --log=tune_lstm --ntrials=50 --batch-size=64 --patience=30 --optimizer=SGD
 elif [ "$1" = "tune_lstm_attention" ]; then
-    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --mode=tune --epochs=500 --model=attention_lstm --log=tune_lstm_attention --ntrials=50 --patience=30 --optimizer=SGD
+    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --mode=tune --epochs=500 --model=attention_lstm --log=tune_lstm_attention --ntrials=50 --patience=60 --optimizer=SGD
 elif [ "$1" = "tune_tcn" ]; then
     CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --mode=tune --epochs=200 --model=tcn --log=tune_tcn --ntrials=50
 # NOTE: before running tuning on frames, set all hyperparameters to tunable=False except for frame_freq, 
@@ -34,7 +34,7 @@ elif [ "$1" = "train_tcn" ]; then
     CUDA_VISIBLE_DEVICES=0 python3 run.py --model=tcn --encode=0 --batch-size=128 --log=tcn --learning-rate=.02022 --epochs=500 --hidden-size=128 --levels 6 --optim=SGD --dropout 0.05
 elif [ "$1" = "train_rnn_attention" ]; then
     # hyperparameters from tuning
-    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --batch-size=16 --log=lstm_att_tuned --learning-rate=0.0391033 --hidden-size=32 --optimizer=SGD --epochs=200 --model=attention_lstm
+    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --batch-size=16 --dropout=0.05 --frame-freq=30 --log=lstm_att_tuned --learning-rate=0.059125 --hidden-size=32 --optimizer=SGD --epochs=500 --model=attention_lstm
 
 ######################################
 # TESTING
