685564841f6993bcc805de73406d699f91732057
Tiffany Cheng <tiffany.ucb@gmail.com>
2020-06-07 22:05:01 +0000

    [results] Enlarged test results

--- results/eval.py
+++ results/eval.py
@@ -14,11 +14,13 @@ def evaluate_performance(true_y, pred_y, print_vals=True):
                   columns = class_names)
     
     # plot confusion matrix with heatmap
+    plt.figure(figsize=(6,4), dpi=200)
     sns.heatmap(cnf_matrix, annot=True, cmap="YlGnBu" ,fmt='g')
     plt.tight_layout()
     plt.title('Confusion matrix', y=1.1)
     plt.ylabel('Actual label')
     plt.xlabel('Predicted label')
+    plt.show()
     
     if print_vals :
         n_accurate = (true_y == pred_y).sum()
