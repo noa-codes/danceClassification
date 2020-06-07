47aa8324aaa1116fb52758e511f558a45c98a0fc
Tiffany Cheng <tiffany.ucb@gmail.com>
2020-06-07 20:11:12 +0000

    [results] Update lstm with attention results

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ if [ "$1" = "pose_encode" ]; then
 elif [ "$1" = "tune_lstm" ]; then
     CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --mode=tune --epochs=500 --model=baseline_lstm --log=tune_lstm --ntrials=100 --batch-size=64 --patience=10 --optimizer=SGD
 elif [ "$1" = "tune_lstm_attention" ]; then
-    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --mode=tune --epochs=500 --model=attention_lstm --log=tune_lstm_attention --ntrials=50 --patience=60 --optimizer=SGD
+    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --mode=tune --epochs=500 --model=attention_lstm --log=tune_lstm_attention --ntrials=100 --patience=30 --optimizer=SGD
 elif [ "$1" = "tune_tcn" ]; then
     CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --mode=tune --epochs=200 --model=tcn --log=tune_tcn --ntrials=50
 elif [ "$1" = "tune_cnn" ]; then
@@ -38,7 +38,7 @@ elif [ "$1" = "train_tcn" ]; then
     CUDA_VISIBLE_DEVICES=0 python3 run.py --model=tcn --encode=0 --batch-size=128 --log=tcn --learning-rate=.02022 --epochs=500 --hidden-size=128 --levels 6 --optim=SGD --dropout 0.05
 elif [ "$1" = "train_lstm_attention" ]; then
     # hyperparameters from tuning
-    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --batch-size=16 --dropout=0.05 --frame-freq=30 --log=lstm_att_tuned --learning-rate=0.059125 --hidden-size=32 --optimizer=SGD --epochs=500 --model=attention_lstm
+    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --batch-size=16 --dropout=0.05 --frame-freq=30 --log=lstm_att_tuned --learning-rate=0.059125 --hidden-size=32 --optimizer=SGD --epochs=500 --mode=train --weight-decay=1e-05 --patience=30 --model=attention_lstm
 elif [ "$1" = "train_cnn" ]; then
     # hyperparameters from tuning
     CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --batch-size=32 --log=cnn --learning-rate=0.01 --hidden-size=256 --optimizer=SGD --epochs=200 --model=baseline_cnn
@@ -51,5 +51,5 @@ elif [ "$1" = "test_lstm" ]; then
 elif [ "$1" = "test_tcn" ]; then
     CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --hidden-size=128 --mode=test --model=tcn --log=test_tcn --levels=6 --checkpoint=/mnt/disks/disk1/log/tcn_2020_6_5_h23_m7_lr0.002187/checkpoints/best_val_loss.pth
 elif [ "$1" = "test_lstm_attention" ]; then
-    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --hidden-size=32  --mode=test --log=test_rnn_att --model=attention_lstm --checkpoint=/mnt/disks/disk1/log/lstm_att_tuned_2020_6_4_h18_m12_lr0.0391033/checkpoints/best_val_loss.pth
+    CUDA_VISIBLE_DEVICES=0 python3 run.py --encode=0 --hidden-size=32  --mode=test --log=test_rnn_att --model=attention_lstm --checkpoint=/mnt/disks/disk1/log/lstm_att_tuned_2020_6_7_h5_m40_lr0.059125/checkpoints/best_val_loss.pth
 fi
